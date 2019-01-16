668083e107ffce2a4834787ef995d1f860841a2a
Alexander <alexander_@hotmail.nl>
2019-01-16 01:44:25 -0800

    List of opening orders example

--- pages/index.vue
+++ pages/index.vue
@@ -5,14 +5,14 @@
         <h1>Marketplace</h1>
       </vs-divider>
       <vs-row 
-        v-if="offers" 
+        v-if="offers.records" 
         vs-align="center" 
         vs-type="flex" 
         vs-justify="space-around" 
         vs-w="12">
         <vs-col
-          v-for="offer in offers"
-          :key="offer.name"
+          v-for="offer in offers.records"
+          :key="offer"
           type="flex"
           vs-justify="center"
           vs-align="center"
@@ -25,13 +25,13 @@
             actionable 
             class="cardx">
             <div slot="header">
-              <h3>{{ offer.title }}</h3>
+              <h3>{{ offer.metaData.title }}</h3>
             </div>
             <div slot="media">
-              <img :src="offer.image">
+              <img :src="offer.metaData.image">
             </div>
             <div>
-              <span class="text-font">{{ offer.desc }}</span>
+              <span class="text-font">{{ offer.metaData.desc }}</span>
             </div>
             <div slot="footer">
               <vs-row vs-justify="flex-end">
@@ -42,18 +42,17 @@
                 <vs-button
                   color="primary"
                   type="gradient"
-                  @click="$router.push('/offer/0x423hlds9f02fjpdas8')"
+                  @click="$router.push('/offer/' + offer.order.makerAddress)"
                 >More details</vs-button>
               </vs-row>
             </div>
           </vs-card>
         </vs-col>
       </vs-row>
-      <vs-prompt
-        :vs-active.sync="activePrompt"
-        @vs-accept="acceptAlert"
-        @vs-close="close"
-      >
+      <vs-prompt 
+        :vs-active.sync="activePrompt" 
+        @vs-accept="acceptAlert" 
+        @vs-close="close">
         <div class="prompt">Current price Offer
           <vs-input 
             placeholder="Code" 
@@ -74,66 +73,39 @@ export default {
     return {
       activePrompt: false,
       offers: {
-        1: {
-          title: 'First offer',
-          desc:
-            'This is the description of the offer, this explains what the user wants in return but that could also be done in pictures later on.',
-          image: 'https://0xproject.com/images/token_icons/WETH.png'
-        },
-        2: {
-          title: 'Second offer',
-          desc:
-            'This is the description of the offer, this explains what the user wants in return but that could also be done in pictures later on.',
-          image: 'https://0xproject.com/images/token_icons/WETH.png'
-        },
-        3: {
-          title: 'Third offer',
-          desc:
-            'This is the description of the offer, this explains what the user wants in return but that could also be done in pictures later on.',
-          image: 'https://0xproject.com/images/token_icons/WETH.png'
-        },
-        4: {
-          title: 'Fourth offer',
-          desc:
-            'This is the description of the offer, this explains what the user wants in return but that could also be done in pictures later on.',
-          image: 'https://0xproject.com/images/token_icons/WETH.png'
-        },
-        5: {
-          title: 'Fifth offer',
-          desc:
-            'This is the description of the offer, this explains what the user wants in return but that could also be done in pictures later on.',
-          image: 'https://0xproject.com/images/token_icons/WETH.png'
-        },
-        6: {
-          title: 'Sixth offer',
-          desc:
-            'This is the description of the offer, this explains what the user wants in return but that could also be done in pictures later on.',
-          image: 'https://0xproject.com/images/token_icons/WETH.png'
-        },
-        7: {
-          title: 'Seventh offer',
-          desc:
-            'This is the description of the offer, this explains what the user wants in return but that could also be done in pictures later on.',
-          image: 'https://0xproject.com/images/token_icons/WETH.png'
-        },
-        8: {
-          title: 'Eigth offer',
-          desc:
-            'This is the description of the offer, this explains what the user wants in return but that could also be done in pictures later on.',
-          image: 'https://0xproject.com/images/token_icons/WETH.png'
-        },
-        9: {
-          title: 'Ninth offer',
-          desc:
-            'This is the description of the offer, this explains what the user wants in return but that could also be done in pictures later on.',
-          image: 'https://0xproject.com/images/token_icons/WETH.png'
-        },
-        10: {
-          title: 'Tenth offer',
-          desc:
-            'This is the description of the offer, this explains what the user wants in return but that could also be done in pictures later on.',
-          image: 'https://0xproject.com/images/token_icons/WETH.png'
-        }
+        total: 1,
+        page: 0,
+        perPage: 20,
+        records: [
+          {
+            metaData: {
+              title: 'First offer',
+              desc:
+                'This is the description of the offer, this explains what the user wants in return but that could also be done in pictures later on.',
+              image: 'https://0xproject.com/images/token_icons/WETH.png'
+            },
+            order: {
+              signature:
+                '0x1b4b01392fd7afd2f9901ead9e99885a62f4d005fe735e0a19cf8f37c9aa8218895f7072f9ab92729ac35f2d478542ffdea62ad78d488e5ff0fbeef667d758f9e303',
+              senderAddress: '0x0000000000000000000000000000000000000000',
+              makerAddress: '0x4ae231df684d81de0e85582b0a99154445a3ea2f',
+              takerAddress: '0x0000000000000000000000000000000000000000',
+              makerFee: '0',
+              takerFee: '0',
+              makerAssetAmount: '1',
+              takerAssetAmount: '100000000000000000',
+              makerAssetData:
+                '0x0257179200000000000000000000000061af2aa3f2ee1b35699d8986dab3f421b671269a0000000000000000000000000000000000000000000000000000000000000001',
+              takerAssetData:
+                '0xf47261b0000000000000000000000000d0a1e359811322d97991e03f863a0c30c2cf029c',
+              salt:
+                '54482334814118760009374173240692376400580602373172445140036590366157243598889',
+              exchangeAddress: '0x35dd2932454449b14cee11a94d3674a936d5d7b2',
+              feeRecipientAddress: '0x0000000000000000000000000000000000000000',
+              expirationTimeSeconds: '1547398871'
+            }
+          }
+        ]
       }
     }
   },
